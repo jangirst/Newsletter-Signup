fcb41afa95e615e76349aa534356a1f0a2498990
JanBoeker <naj_b@gmx.de>
2020-10-20 22:15:53 +0200

    file app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -14,15 +15,54 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  console.log(req.body);
-
+  const apiKey = "33734e0ec3a037f35356e534d0af032e-us2";
+  const listID = "81240d6933";
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
 
-  res.sendFile(__dirname + "/success.html");
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us2.api.mailchimp.com/3.0/lists/" + listID;
+  const options = {
+    method: "POST",
+    auth: "naj__b:" + apiKey
+  };
+
+  const request = https.request(url, options, function(response){
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
+// Try-again-button
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running.");
 });
